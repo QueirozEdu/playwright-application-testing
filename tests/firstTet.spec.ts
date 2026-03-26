1f44e43202fb45e6a436dcd498ed2204e9b7b6a4
Eduardo Queiroz <eduhmq@gmail.com>
2026-03-25 23:12:13 -0300

    add beforeEach and text validation

--- tests/firstTet.spec.ts
+++ tests/firstTet.spec.ts
@@ -1,7 +1,17 @@
 import { test } from '@playwright/test';
 
-test('the first test', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4200');
-  await page.getByText('Forms').click();
-  await page.getByText('Form Layouts').click();
+});
+
+test.describe('Validade homepage', () => {
+  test('Sidebar buttons are clickable', async ({ page }) => {
+    await page.getByText('Forms').click();
+    await page.getByText('Form Layouts').click();
+    await page.getByText('Datepicker').click();
+    await page.getByText('Modal & Overlays').click();
+    await page.getByText('Extra Components').click();
+    await page.getByText('Tables & Data').click();
+    await page.getByText('Auth').click();
+  });
 });
