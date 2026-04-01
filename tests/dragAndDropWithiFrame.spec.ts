4875ba148766e4243f6dbb77ce08e995e8a33809
Eduardo Queiroz <eduhmq@gmail.com>
2026-03-31 22:02:24 -0300

    add tests drag and drop by mouse movements

--- tests/dragAndDropWithiFrame.spec.ts
+++ tests/dragAndDropWithiFrame.spec.ts
@@ -9,4 +9,15 @@ test('Drag and drop with iFrame', async ({ page }) => {
   await frame
     .locator('li', { hasText: 'High Tatras 2' })
     .dragTo(frame.locator('#trash'));
+
+  //more precise control
+  await frame.locator('li', { hasText: 'High Tatras 4' }).hover();
+  await page.mouse.down();
+  await frame.locator('#trash').hover();
+  await page.mouse.up();
+
+  await expect(frame.locator('#trash li h5')).toHaveText([
+    'High Tatras 2',
+    'High Tatras 4',
+  ]);
 });
